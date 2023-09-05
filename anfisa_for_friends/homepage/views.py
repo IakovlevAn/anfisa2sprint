a6219fd492aaf3c954dadfe86a8786abaddabfa3
IakovlevAn <ifdyttl@gmail.com>
2023-09-06 02:29:09 +0300

    changed static

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+
+    ice_cream_list = IceCream.objects.select_related('category')
+
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
